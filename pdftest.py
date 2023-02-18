60ce14ea23570313a497721c1bd9f98a73f0c1b3
SLong314159 <92769901+SLong314159@users.noreply.github.com>
2023-02-18 00:06:58 -0500

    Update pdftest.py

--- pdftest.py
+++ pdftest.py
@@ -9,17 +9,25 @@
 #    url = "https://arxiv.org/abs/1908.10084",
 #)
 
-from pdfminer.high_level import extract_text_to_fp # Install pdfminer
-from pdfminer.layout import LAParams
-from bs4 import BeautifulSoup # Install bs4
+# Required Packages
+# Python 3.9
+# -U sentence-transformers
+# -U nltk
+# torch
+
+# Unused Models tested:
+# all-MiniLM-L6-v2
+
+import nltk
 import nltk.data
 import logging
 from io import StringIO
 import sys
 import csv
 import matplotlib.pyplot as plt
-from sentence_transformers import SentenceTransformer, util # pip install -U sentence-transformers
-model = SentenceTransformer('all-MiniLM-L6-v2')
+from sentence_transformers import SentenceTransformer, util
+model = SentenceTransformer('all-mpnet-base-v2')
+model_data = 'tokenizers/punkt/english.pickle'
 
 logger = logging.getLogger(__name__)
 show_progress_bar = (logger.getEffectiveLevel()==logging.INFO or logger.getEffectiveLevel()==logging.DEBUG)
@@ -32,48 +40,28 @@ def main():
 	ext = dataFile[-4:]
 
 	# Verify file extension
-	if ext == '.pdf':
-		# Convert PDF to HTML
-		output_string = StringIO()
-		with open(dataFile, 'rb') as fin:
-			extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='html', codec=None)
-
-		# Convert HTML to Plain Text
-		htmlOutput = output_string.getvalue().strip()
-		textOutput = BeautifulSoup(htmlOutput, features='html.parser')
-
-		result = textOutput.get_text()
-
-	elif ext == '.txt':
+	if ext == '.txt':
 		# Read Text File
 		fp = open(dataFile)
 		result = fp.read()
 		# TEST LINE #
 		#print('\n\n'.join(tokenizer.tokenize(data)))
-
 	else:
 		print('Invalid File.')
 		exit()
 	
 	# Define pre-set data model
-	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+	# Test other models
+	tokenizer = nltk.data.load(model_data)
 	# Convert text to sentence array
 	sentences = tokenizer.tokenize(result)
-
-	# TEST LINE PLEASE REMOVE #
-	# print('\nSentences:')
-	# print('\n\n'.join(sentences))
 
 	#Read symptoms text file and convert to sentence array
 	fps = open('symptoms.txt')
 	results = fps.read()
-	tokenizers = nltk.data.load('tokenizers/punkt/english.pickle')
+	tokenizers = nltk.data.load(model_data)
 	symptoms = tokenizers.tokenize(results)
 	
-	# TEST LINE PLEASE REMOVE #
-	# print('\nSymptoms:')
-	# print('\n\n'.join(symptoms))
-
 	#Encode sentence array(s)
 	embeddings = model.encode(sentences)
 	embeddings2 = model.encode(symptoms)
@@ -91,17 +79,17 @@ def main():
 	all_sentence_combinations = sorted(all_sentence_combinations, key=lambda x: x[0], reverse=True)
 
 	print("\nGenerated Report: \n")
-	innatentive_Score = 5
-	hyperactive_Score = 9
+	innatentive_Score = 0
+	hyperactive_Score = 0
 	for score, k, l in all_sentence_combinations:
 		# Checks for relevant sentences using a metric of 36%
 		# Symptoms Index: 0-15 = Innatentive, 16+ = Hyperactive
-		if cos_sim[k][l] >= 0.36:
+		if cos_sim[k][l] > 0.50:
 			if symptoms.index(symptoms[l]) <= 15:
 				innatentive_Score+=1
 			elif symptoms.index(symptoms[l]) >=16:
 				hyperactive_Score+=1
-			print("{} \t {} \t {} \n".format(sentences[k], symptoms[l], cos_sim[k][l]))
+			print("{} | {} | Score: {} \n".format(sentences[k], symptoms[l], cos_sim[k][l]))
 
 	# Calculate the dominant ADHD type from the given symptoms, and print results
 	total_Score = innatentive_Score + hyperactive_Score
@@ -113,7 +101,6 @@ def main():
 	plt.pie(report_data, labels=report_labels, colors =report_colors, startangle = 90, autopct='%1.1f%%')
 	plt.title('Testemonial Results Breakdown')
 	plt.legend()
-	# plt.text(-2, -1, resulting_text, size=12, ha="left", va="bottom", bbox=dict(facecolor='white'))
 	plt.show()
 
 if __name__ == "__main__":
